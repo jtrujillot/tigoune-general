d44d7cf30e160b5062e8f528e3241d7432e48cc8
Jaime Trujillo <jtrujillot@intergrupo.com>
2016-12-29 10:40:25 -0500

    Adición de cambios de authorization

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use TigoUne\General\Bundles\AuthorizationBundle\Model\Authorization;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+		$authorization = new Authorization();
+		$authorization->setSuscriberid("1");
+		$authorizationRepository = $this->get("tigoune_general.authorization.autorization_repository");
+		$value = $authorizationRepository->validateUrnBySuscriber($authorization);
+		var_dump($value);
+		die();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
